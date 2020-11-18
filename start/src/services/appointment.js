1ee9ab5efbae513775e73a6480750c2b373432c8
Henry Roeland <henry@roeland.net>
2020-11-18 10:11:41 +0100

    Adding dependencies needed for later Steps
    - Added Vuex and VueRouter and lodash(For debounce)
    - Added routing and store directory with prepared code

--- start/src/services/appointment.js
+++ start/src/services/appointment.js
@@ -1,51 +1,4 @@
-import moment from "moment";
-
-export function getItemsPerMonth(year, month) {
-  var n = getIsoWeekDaysInMonth(year, month);
-
-  var arr = Array.from(
-    {
-      length: n
-    },
-    (x, i) =>
-      moment(year + "-" + month + "-01", "YYYY-MM-DD")
-        .startOf("isoWeek")
-        .add(i, "days")
-  );
-
-  const reqMonthId = moment(year + "-" + month + "-01", "YYYY-MM-DD").monthId();
-  return arr.map(x => {
-    return {
-      date: x,
-      labelCount: 0,
-      isInReqMonth: reqMonthId === x.monthId()
-    };
-  });
-}
-
-export function getFullMonthName(month) {
-  return moment("2020-" + month + "-01", "YYYY-MM-DD").format("MMMM");
-}
-
-export function getIsoWeekDaysInMonth(year, month) {
-  var momentObj = moment(year + "-" + month + "-01", "YYYY-MM-DD");
-
-  var mFirstIsoWeekDayOfMonth = momentObj
-    .clone()
-    .startOf("month")
-    .startOf("isoWeek");
-  var mLastIsoWeekDayOfMonth = momentObj
-    .clone()
-    .endOf("month")
-    .endOf("isoWeek");
-
-  var d = moment.duration(mLastIsoWeekDayOfMonth.diff(mFirstIsoWeekDayOfMonth));
-
-  return d.asDays() + 1;
-}
-
-export function getAppointments() {
-  return [
+const fakeResponse = [
     {
       id: 1,
       date: new Date("2020-03-12 18:00"),
@@ -126,8 +79,10 @@ export function getAppointments() {
       important: false
     },        
   ];
-}
 
-moment.prototype.monthId = function() {
-  return Number(this.format("YYYYMM"));
-};
+  export default {
+      async list() {
+          return fakeResponse;
+      }
+  };
+  
